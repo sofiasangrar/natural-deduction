f8569875db9f3796f489d8fa7c67831f9c9eeab9
sofiasangrar <sofiasangrar@hotmail.co.uk>
2022-03-09 16:42:58 +0000

    last comments

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -7,25 +7,22 @@ public final class Parser {
   public static boolean error = false;
   public static RuntimeException exception = null;
 
+    /**
+     * remove errors from parser
+     */
   public static void clearError(){
       error = false;
       exception = null;
   }
 
-  static void errorHandle() {
-      error = true;
-      exception = new SyntaxError();
-  }
-
-    static void errorHandle(String expected) {
+    /**
+     * when the wrong string is found, create a new error
+     * @param expected the expected value to be found
+     */
+  static void errorHandle(String expected) {
         error = true;
         exception = new SyntaxError(expected, t.expectedString(), t.getStartingColumnOfToken());
-    }
-
-  static void errorHandle(String expected, String actual, int charNo) {
-        error = true;
-        exception = new SyntaxError(expected, actual, charNo);
-    }
+  }
 
 
 }
